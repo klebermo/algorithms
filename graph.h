1da9cb6727ccf5782880bc9519ad00b1a68e95bb
Kleber Mota de Oliveira <klebermo@gmail.com>
2023-05-01 17:16:58 -0300

    Update 01/05/2023 17:16

--- graph.h
+++ graph.h
@@ -4,6 +4,10 @@
 #include "list.h"
 #include "edge.h"
 
+#include <unordered_map>
+#include <stack>
+#include <queue>
+
 template<class T>
 class Graph {
 private:
@@ -21,7 +25,31 @@ public:
   void add_edge(T x, T y, int weight = 1, float angle = 1.0f);
   void remove_edge(T x, T y);
 
-  List<Vertex<T>> search(T origin, T destination);
+  List<T> searchDepthFirst(T origin, T destination);
+  List<T> searchBreathFirst(T origin, T destination);
+  List<T> searchAlphaBetaPruning(T origin, T destination);
+  List<T> searchAStar(T origin, T destination);
+  List<T> searchBStar(T origin, T destination);
+  List<T> searchDStar(T origin, T destination);
+  List<T> searchSSSStar(T origin, T destination);
+  List<T> searchBestFirst(T origin, T destination);
+  List<T> searchBeam(T origin, T destination);
+  List<T> searchBidirecional(T origin, T destination);
+  List<T> searchFringe(T origin, T destination);
+  List<T> searchJumpPoint(T origin, T destination);
+  List<T> searchMonteCarloTree(T origin, T destination);
+
+  List<T> searchBellmanFord(T origin, T destination);
+  List<T> searchDijkstra(T origin, T destination);
+  List<T> searchFloydWarshall(T origin, T destination);
+  List<T> searchJohnson(T origin, T destination);
+  List<T> searchShortestPathFaster(T origin, T destination);
+  List<T> searchYen(T origin, T destination);
+
+  List<T> searchBoruvka(T origin, T destination);
+  List<T> searchKruskal(T origin, T destination);
+  List<T> searchPrim(T origin, T destination);
+  List<T> searchReverseDelete(T origin, T destination);
 
   Vertex<T> get_vertex(T x);
   Edge<T> get_edge(T x, T y);
@@ -108,11 +136,231 @@ void Graph<T>::remove_edge(T x, T y) {
         vx.getEdges()->remove(i);
     }
   }
+
+  max = vy.getEdges()->size();
+  for(int i=1; i<max; i++) {
+    Edge<T> e = vy.getEdges()->get(i)->getData();
+    for(auto j : e.getVertices()) {
+      Vertex<T> v = j->second;
+      if(v == vx)
+        vy.getEdges()->remove(i);
+    }
+  }
 }
 
 template<class T>
-List<Vertex<T>> Graph<T>::search(T origin, T destination) {
-  List<Vertex<T>> result;
+List<T> Graph<T>::searchDepthFirst(T origin, T destination) {
+  List<T> visited;
+  std::unordered_map<T, bool> visitedMap;
+  std::stack<Vertex<T>> stack;
+
+  // inicializa o mapa de visitados
+  for (int i=1; i<=vertices.size(); i++) {
+    Vertex<T> vertex = vertices.get(i)->getData();
+    visitedMap[vertex.getData()] = false;
+  }
+
+  // adiciona o vértice inicial na pilha e o marca como visitado
+  stack.push(get_vertex(origin));
+  visitedMap[origin] = true;
+
+  // inicia o algoritmo DFS
+  while (!stack.empty()) {
+    Vertex<T> current = stack.top();
+    stack.pop();
+
+    visited.insert(current.getData()); // adiciona o vértice atual à lista de visitados
+
+    // verifica se o destino foi alcançado
+    if (current.getData() == destination) {
+      break;
+    }
+
+    // percorre todos os vizinhos do vértice atual
+    List<Vertex<T>> neighbors_list = neighbors(current.getData());
+    for (int i=1; i<=neighbors_list.size(); i++) {
+      Vertex<T> neighbor = neighbors_list.get(i)->getData();
+      T neighborData = neighbor.getData();
+
+      // visita o vizinho, caso ainda não tenha sido visitado
+      if (!visitedMap[neighborData]) {
+        visitedMap[neighborData] = true; // marca o vizinho como visitado
+        stack.push(neighbor); // adiciona o vizinho na pilha
+      }
+    }
+  }
+
+  return visited;
+}
+
+template<class T>
+List<T> Graph<T>::searchBreathFirst(T origin, T destination) {
+  List<T> visited;
+  std::unordered_map<T, bool> visitedMap;
+  std::queue<Vertex<T>> queue;
+
+  // inicializa o mapa de visitados
+  for (int i=1; i<=vertices.size(); i++) {
+    Vertex<T> vertex = vertices.get(i)->getData();
+    visitedMap[vertex.getData()] = false;
+  }
+
+  // adiciona o vértice inicial na fila e o marca como visitado
+  queue.push(get_vertex(origin));
+  visitedMap[origin] = true;
+
+  // inicia o algoritmo BFS
+  while (!queue.empty()) {
+    Vertex<T> current = queue.front();
+    queue.pop();
+
+    visited.insert(current.getData()); // adiciona o vértice atual à lista de visitados
+
+    // verifica se o destino foi alcançado
+    if (current.getData() == destination) {
+      break;
+    }
+
+    // percorre todos os vizinhos do vértice atual
+    List<Vertex<T>> neighbors_list = neighbors(current.getData());
+    for (int i=1; i<=neighbors_list.size(); i++) {
+      Vertex<T> neighbor = neighbors_list.get(i)->getData();
+      T neighborData = neighbor.getData();
+
+      // visita o vizinho, caso ainda não tenha sido visitado
+      if (!visitedMap[neighborData]) {
+        visitedMap[neighborData] = true; // marca o vizinho como visitado
+        queue.push(neighbor); // adiciona o vizinho na fila
+      }
+    }
+  }
+
+  return visited;
+}
+
+template<class T>
+List<T> Graph<T>::searchAlphaBetaPruning(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchAStar(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchBStar(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchDStar(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchSSSStar(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchBestFirst(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchBeam(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchBidirecional(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchFringe(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchJumpPoint(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchMonteCarloTree(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchBellmanFord(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchDijkstra(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchFloydWarshall(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchJohnson(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchShortestPathFaster(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchYen(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchBoruvka(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchKruskal(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchPrim(T origin, T destination) {
+  List<T> result;
+  return result;
+}
+
+template<class T>
+List<T> Graph<T>::searchReverseDelete(T origin, T destination) {
+  List<T> result;
   return result;
 }
 
